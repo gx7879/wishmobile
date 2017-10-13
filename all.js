294020ccc52b66dcc6f82b3ffa764778a839246e
gx7879 <gx7879@gmail.com>
2017-10-13 16:55:59 +0800

    17/10/13

---
+++ all.js
@@ -0,0 +1,24 @@
+
+// function cssresize(){
+// 	var mbs = document.querySelector('.mobileservice');
+// 	var value = "";
+// 	if (window.innerWidth > 768) {
+// 		if (mbs.length <= 2) {
+// 			value = '0px';
+// 		}
+// 		mbs.style.marginBottom = value;
+// 	}else{
+// 		if (mbs.length <= 2) {
+// 			value = '10px';
+// 		}
+// 		mbs.style.marginBottom = value;
+// 	}
+// }
+
+// window.addEventListener('load',function(){
+// 	cssresize();
+// });
+
+// window.addEventListener('resize',function(){
+// 	cssresize();
+// });
